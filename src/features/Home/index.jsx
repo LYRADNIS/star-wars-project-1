657713b39aaa7bc38e6c16d620f1c42bd31eb356
Joseph Lin <josephlin55555@gmail.com>
2018-07-07 10:35:23 -0400

    [replace] react with bounty hunter favicon

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 class Home extends Component {
+    formatDate(date) {
+        return moment(date).format('dddd, MMMM Do YYYY').toString();
+    }
+
     render() {
         return (
             <div>
                 {
                     this.props.films
-                        .map(({title}, key) =>
-                            <div key={ key }>{ title }</div>
+                        .map(({title, release_date}, key) =>
+                            <div key={ key }>{ title } { this.formatDate(release_date) }</div>
                         )
                 }
             </div>
