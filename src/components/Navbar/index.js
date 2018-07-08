d3709c18172d312bf25e974d4032d5ddb4e94db8
Joseph Lin <josephlin55555@gmail.com>
2018-07-08 00:05:07 -0400

    [bootstrap] application

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 import { clickCharacter } from './../../actions';
 
 class Navbar extends Component {
+    assignButtonClass(name) {
+        let buttonClass = '';
+
+        const isDarthVader = name === 'Darth Vader'
+        const isLukeSkywalker = name === 'Luke Skywalker';
+        const isObiwan = name === 'Obi-wan Kenobi';
+        const isR2 = name === 'R2-D2';
+
+        if (isDarthVader) {
+            buttonClass = 'btn-outline-danger';
+        } else if (isLukeSkywalker) {
+            buttonClass = 'btn-outline-success';
+        } else if (isObiwan) {
+            buttonClass = 'btn-outline-primary';
+        } else if (isR2) {
+            buttonClass = 'btn-outline-warning';
+        }
+
+        return buttonClass;
+    }
+
     render() {
         return (
             <div className="standard-row-height border-bottom-black row center-xs middle-xs">
@@ -10,7 +32,7 @@ class Navbar extends Component {
                     this.props.characters
                         .map(({name, url}, key) =>
                             <div className="col-xs" onClick={ () => clickCharacter(url) } key={ key }>
-                                { name }
+                                <button className={ classnames('btn', this.assignButtonClass(name)) }>{ name }</button>
                             </div>
                         )
                 }
